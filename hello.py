c5ee4980ddea85b39dbf914476f2c43e09486dcd
O-Seok <totkfa789@gmail.com>
2020-05-11 11:45:42 +0900

    feat: add conditional statements print only v is even

--- hello.py
+++ hello.py
@@ -1,11 +1,5 @@
 print('hello, world !')
 print('---------------')
 for v in range(0, 5+1):
-  print('hello, world !')
-print('---------------')
-p = range(1 , 5+1)
-print(p)
-for v in p:
-  print('hello, world !', v)
-  if v / 2 == 0:
+  if v % 2 == 0:
     print('hello, world !')
