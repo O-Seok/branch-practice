4e62a6918aceb2daf6ec4854ac3f5fd5d923ba47
O-Seok <totkfa789@gmail.com>
2020-05-11 11:43:36 +0900

    feat: iterate 5 times

--- hello.py
+++ hello.py
@@ -1,4 +1,11 @@
 print('hello, world !')
 print('---------------')
-for v in range(0, 5):
+for v in range(0, 5+1):
   print('hello, world !')
+print('---------------')
+p = range(1 , 5+1)
+print(p)
+for v in p:
+  print('hello, world !', v)
+  if v / 2 == 0:
+    print('hello, world !')
